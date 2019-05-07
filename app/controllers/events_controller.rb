4b0860dd9e1e8b2fe506cf6699900526e62167e1
Julie K <juliette.nada@gmail.com>
2019-05-07 17:05:05 +0200

    event creation works

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,5 +13,30 @@ class EventsController < ApplicationController
   def new
   end
 
+  def create
+    permitted = params.permit(:location,
+    :start_time, :duration, :price, :title, :description)
+    p "#"*50
+    p params
+    p "#"*50
+    p permitted
+    p "#"*50
+    new_event = Event.create(location: permitted[:location],
+    start_time: permitted[:start_time],
+    duration: permitted[:duration],
+    admin_id: current_user.id,
+    title: permitted[:title],
+    description: permitted[:description],
+    price: permitted[:price])
+
+    if new_event.valid?
+      flash[:success] = "Your event is created. Check it out!"
+      redirect_to '/'
+    else
+      flash[:danger] = "#{new_event.errors.details}. Please try again."
+      render 'new'
+    end
+  end
+
 
 end
