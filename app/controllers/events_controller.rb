334cbb32df8278d6f847f97311029c8e143bc96f
Julie K <juliette.nada@gmail.com>
2019-05-07 16:24:28 +0200

    deleted a duplated controller. events index and show views ok

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,13 @@
 class EventsController < ApplicationController
+  attr_reader :events
+
   def index
     @events = Event.all
   end
 
   def show
+    @id = params[:id].to_i
+    @event = Event.find(@id)
   end
 
   def new
