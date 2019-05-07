acf229b1e0dcde9537d0ac9690b0aa82183c7876
Julie K <juliette.nada@gmail.com>
2019-05-07 13:21:12 +0200

    sendgrid id added to .env/.env added to .gitignore

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
   ############################# for Devise
   root to: "home#index"
+
+
+
+  devise_for :users
   resources :users
   resources :events
   resources :attendances
