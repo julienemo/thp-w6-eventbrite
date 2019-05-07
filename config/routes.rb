c1738d8e16433f71675b9961f753692bc3a65ea2
Julie K <juliette.nada@gmail.com>
2019-05-07 14:10:09 +0200

    static pages added. now the notion of logged in is clear. sign in and sign up links in header settled

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/index'
+  get 'static_pages/secret'
   ############################# for Devise
-  root to: "home#index"
+  root to: "static_pages#index"
 
 
 
