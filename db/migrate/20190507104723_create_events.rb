07b1f09fded0efcda19879d1677338369f053410
Julie K <juliette.nada@gmail.com>
2019-05-07 13:03:07 +0200

    table creation + devise to user migrations done

--- db/migrate/20190507104723_create_events.rb
+++ db/migrate/20190507104723_create_events.rb
@@ -1,7 +1,13 @@
 class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
-
+      t.references :admin, index: true
+      t.datetime :start_time
+      t.integer :duration
+      t.string :title
+      t.text :description
+      t.integer :price
+      t.string :location
       t.timestamps
     end
   end
--- db/migrate/20190507104728_create_attendances.rb
+++ db/migrate/20190507104728_create_attendances.rb
@@ -1,7 +1,9 @@
 class CreateAttendances < ActiveRecord::Migration[5.2]
   def change
     create_table :attendances do |t|
-
+      t.references :event, foreign_key: true
+      t.references :participant, index: true
+      t.string :stripe_customer_id
       t.timestamps
     end
   end
