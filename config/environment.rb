d0b8df7843b07d3ca4e2af1c13bd732b3b517466
Julie K <juliette.nada@gmail.com>
2019-05-07 18:43:30 +0200

    mailer can really be sent coz tested

--- config/environment.rb
+++ config/environment.rb
@@ -5,16 +5,16 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_LOGIN'],
-  :password => ENV['SENDGRID_PWD'],
-  :domain => 'gmail.com',
-  :address => 'smtp.sendgrid.net',
-  :enable_starttls_auto => true
+#  :user_name => ENV['SENDGRID_LOGIN'],
+#  :password => ENV['SENDGRID_PWD'],
+#  :domain => 'gmail.com',
+#  :address => 'smtp.sendgrid.net',
+#  :enable_starttls_auto => true
 
-#  :address            => 'smtp.gmail.com',
-#  :port               => 587,
-#  :domain             => 'gmail.com', #you can also use google.com
-#  :authentication     => :plain,
-#  :user_name          => ENV['GMAIL_LOGIN'],
-#  :password           => ENV['GMAIL_PWD']
+  :address            => 'smtp.gmail.com',
+  :port               => 587,
+  :domain             => 'gmail.com', #you can also use google.com
+  :authentication     => :plain,
+  :user_name          => ENV['GMAIL_LOGIN'],
+  :password           => ENV['GMAIL_PWD']
 }
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   ############################ for leter opener
-  config.action_mailer.delivery_method = :letter_opener
+  #config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
 
