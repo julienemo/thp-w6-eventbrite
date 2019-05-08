3c93a4d3078948a8c103f6ae84de3291e655164f
Julie K <juliette.nada@gmail.com>
2019-05-08 22:43:25 +0200

    added attendance validation: when event is free, absolutely no stripe id, otherwise, must have a stripe id

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,6 +1,7 @@
 class Attendance < ApplicationRecord
   after_create :send_confirmation
   validates_uniqueness_of :participant_id, :scope => [:event_id]
+  validate :stripe_id_coherent?
 
   belongs_to :event
 
@@ -12,4 +13,11 @@ class Attendance < ApplicationRecord
     AttendanceMailer.participation_confirmation(self).deliver_now
   end
 
+  private
+  def stripe_id_coherent?
+    e_price = Event.find(event_id).price
+    unless ((e_price == 0 && stripe_customer_id == nil)||(e_price !=0 && stripe_customer_id != nil))
+      errors.add(:strip_customer_id, "can only be empt for free events.")
+    end
+  end
 end
