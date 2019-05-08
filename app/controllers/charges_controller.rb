e45b0b3fc032710e8919f084647ecbc3326a199a
Julie K <juliette.nada@gmail.com>
2019-05-08 22:06:15 +0200

    link join button from event show view to payment

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,25 +1,37 @@
 class ChargesController < ApplicationController
+  before_action :get_current_event, :get_amount, :only =>[:create, :new]
+
   def new
-end
+  end
 
-def create
-  # Amount in cents
-  @amount = 500
+  def create
+    customer = Stripe::Customer.create({
+      email: params[:stripeEmail],
+      source: params[:stripeToken],
+    })
 
-  customer = Stripe::Customer.create({
-    email: params[:stripeEmail],
-    source: params[:stripeToken],
-  })
+    charge = Stripe::Charge.create({
+      customer: customer.id,
+      amount: @amount*100,
+      description: 'Event entry',
+      currency: 'EUR',
+    })
+    flash[:success] = "Payment of #{@amount} succeed, you are officially on the guest list of event #{@event.id}"
+    redirect_to event_path(@event.id)
+  rescue Stripe::CardError => e
+    flash[:danger] = e.message
+    redirect_to '/'
+  rescue Exception => e
+    flash[:danger] = e.message
+    redirect_to '/'
+  end
 
-  charge = Stripe::Charge.create({
-    customer: customer.id,
-    amount: @amount,
-    description: 'Rails Stripe customer',
-    currency: 'usd',
-  })
+  private
+  def get_current_event
+    @event = Event.find(params[:event_id])
+  end
 
-rescue Stripe::CardError => e
-  flash[:error] = e.message
-  redirect_to new_charge_path
-end
+  def get_amount
+    @amount = @event.price
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,14 @@
 Rails.application.routes.draw do
-  get 'event_space', to: 'static_pages#event_space'
-  get 'static_pages/secret'
   ############################# for Devise
   root to: "events#index"
-
-
-
   devise_for :users
+  ############################# Devise over
+
   resources :users
-  resources :charges
+
   resources :events do
     resources :attendances
+    resources :charges
   end
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
