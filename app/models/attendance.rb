b4ded1b3c238d8e73dd38d0632c3d658059ffa44
Julie K <juliette.nada@gmail.com>
2019-05-08 22:53:06 +0200

    added attendance validation: admin can't participate in own managed events

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,6 +2,7 @@ class Attendance < ApplicationRecord
   after_create :send_confirmation
   validates_uniqueness_of :participant_id, :scope => [:event_id]
   validate :stripe_id_coherent?
+  validate :admin_cant_participate
 
   belongs_to :event
 
@@ -18,6 +19,13 @@ class Attendance < ApplicationRecord
     e_price = Event.find(event_id).price
     unless ((e_price == 0 && stripe_customer_id == nil)||(e_price !=0 && stripe_customer_id != nil))
       errors.add(:strip_customer_id, "can only be empt for free events.")
+    end
+  end
+
+  def admin_cant_participate
+    e_admin = Event.find(event_id).admin_id
+    if e_admin == participant_id
+      errors.add(:participant_id, "Admin can't participate in own event.")
     end
   end
 end
