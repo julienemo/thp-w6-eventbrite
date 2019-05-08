08d71e659db4fabdeeb424f9b51f6e06fe157af3
Julie K <juliette.nada@gmail.com>
2019-05-08 17:28:29 +0200

    added stripe charges controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,9 @@
-#Attendance.destroy_all
+# visibly the confirmation mails are blocking seed
+Rails.application.configure do
+  config.action_mailer.perform_deliveries = false
+end
+
+Attendance.destroy_all
 #Event.destroy_all
 #User.destroy_all
 separator = "-*" * 40
@@ -8,12 +13,28 @@ puts ""
 puts "Previous record deleted."
 
 
+users = User.all.map{|u| u.id}
+events = Event.all.map{|e| e}
+users.each do |u|
+  events.each do |e|
+    unless e.admin_id == u
+      Attendance.create(event_id: e.id, participant_id: u)
+    end
+  end
+end
+puts "#{Attendance.all.length} participations created. Everybody has participated in all events not created by oneself."
+puts ""
+puts separator
+
+
+=begin
 3.times do
   User.create(first_name: Faker::Name.first_name,
   email: Faker::Internet.email,
   password: Faker::Code.nric)
 end
 puts "#{User.all.length} fake user profiles created."
+
 
 users = User.all.map{|u| u.id}
 users.each do |u|
@@ -28,15 +49,4 @@ users.each do |u|
   end
 end
 puts "#{Event.all.length} events created, 2 per admin."
-
-events = Event.all.map{|e| e}
-users.each do |u|
-  events.each do |e|
-    unless e.admin_id == u
-      Attendance.create(event_id: e.id, participant_id: u)
-    end
-  end
-end
-puts "#{Attendance.all.length} participations created. Everybody has participated in all events not created by oneself."
-puts ""
-puts separator
+=end
