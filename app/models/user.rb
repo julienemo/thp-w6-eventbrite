42f9ffc35a53f015520d2ac7fa0e98cc64c2c887
Julie K <juliette.nada@gmail.com>
2019-05-09 13:30:49 +0200

    in user model, specified that each user has one avatar attached

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,27 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         after_create :send_welcome
+  after_create :send_welcome
 
-       has_many :managed_events,
-         foreign_key: 'admin_id',
-         class_name: "Event",
-         dependent: :destroy
+  has_one_attached :avatar
 
-       has_many :attendances,
-         foreign_key: 'participant_id'
+  has_many :managed_events,
+    foreign_key: 'admin_id',
+    class_name: "Event",
+    dependent: :destroy
 
-       has_many :participants,
-         foreign_key: 'participant_id',
-         through: :managed_events,
-         class_name: 'User',
-         dependent: :destroy
+  has_many :attendances,
+    foreign_key: 'participant_id'
 
-       def send_welcome
-           UserMailer.welcome_email(self).deliver_now
-       end
+  has_many :participants,
+    foreign_key: 'participant_id',
+    through: :managed_events,
+    class_name: 'User',
+    dependent: :destroy
+
+  def send_welcome
+    UserMailer.welcome_email(self).deliver_now
+  end
 
 end
