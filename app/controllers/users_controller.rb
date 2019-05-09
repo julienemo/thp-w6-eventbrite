71cef8295097f7404d56f2142b028e8f546f4ac2
Julie K <juliette.nada@gmail.com>
2019-05-09 20:49:30 +0200

    user can now update first/last name and description

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,19 @@ class UsersController < ApplicationController
   end
 
   def update
+    current_user.update(user_attributes)
+    if current_user.valid?
+      flash[:success] = "Profile updated. Check it out!"
+      redirect_to user_path(current_user.id)
+    else
+      flash[:danger] = "Update failed. Please try again."
+      render 'edit'
+    end
   end
 
+  private
+  def user_attributes
+    permitted = params.permit(:first_name,
+    :last_name, :description)
+  end
 end
