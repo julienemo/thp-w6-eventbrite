508d1b73cd91dcea580bfa3b60c8cf39fde13cfe
Julie K <juliette.nada@gmail.com>
2019-05-10 10:46:54 +0200

    corrected a comment typo

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   ############################ for devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  ############################ for leter opener
+  ############################ for letter opener
   #config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
